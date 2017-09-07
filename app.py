ac14f7b44b2bd08d512e1cecb8292d8dd5a5efb8
Harlin Seritt <harlin.seritt@gmail.com>
2017-09-07 11:24:05 -0600

    Added some comments

--- app.py
+++ app.py
@@ -26,14 +26,23 @@ for row in results:
 	# Uncomment below to see all property keys and values
 	#print row.properties
 	print
+	# Will give the name of the document:
 	print 'Name: {}'.format(row.properties['cmis:name'])
+	
+	# Shows noderef which can be used with any public Alfresco API
 	print 'NodeRef: {}'.format(row.properties['alfcmis:nodeRef'])
+
+	# Shows the user who created the document (creater is also owner)
 	print 'Owner: {}'.format(row.properties['cmis:createdBy'])
+
+	# Shows last modified date and last modified by and latest version of document.
 	print 'Last Mod Date: {}'.format(row.properties['cmis:lastModificationDate'])
 	print 'Last Mod By: {}'.format(row.properties['cmis:lastModifiedBy'])
 	print 'Latest Version: {}'.format(row.properties['cmis:versionLabel'])
 
+	# Get the document object so we can get the path and the available ACLs for it.
 	doc = repo.getObject(row.properties['cmis:objectId'])
+
 	try:
 		print 'Paths: {}'.format(doc.getPaths()[0])
 	except IndexError:
